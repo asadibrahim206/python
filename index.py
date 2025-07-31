970b4b6374c37d01de2d73fae07022592353b9fc
asad ibrahim <asad.denin@gmail.com>
2025-07-31 14:22:36 +0500

    changes have been made

---
+++ index.py
@@ -0,0 +1,37 @@
+# gander = input("Enter your Gander:")
+# if gander == "m":
+#     print("good morning SIR")
+# elif gander == "f":
+#     print("good morning MADAM")
+
+
+#       QUESTION : 2
+# EVEN AND ODD NUMBER
+# number = int(input("enter a number :"))
+# if number % 2 == 0:
+#     print(f"{number} is an even number")
+# elif number % 2 != 0:
+#     print(f"{number} is not an even number")
+
+
+
+#       QUESTION : 3
+# VALID VOTER OR not
+# name = input("please enter your name : ")
+# age = int(input("please enter your age :"))
+
+# if age > 18:
+#     print(f"{name} is a valid voter")
+# elif age < 18 and age > 0:
+#     print(f"{name} is not a valid voter")
+# else:
+#     print("invalid input")
+
+#       QUESTION : 4
+# Accept a year and check weather it is leap or not 
+alphabet = input("enter your char:")
+if alphabet in "aeiouAEIOU":
+    print(f"{alphabet} is  Vowel")
+else:
+    print("consonent")
+
