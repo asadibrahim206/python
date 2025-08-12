827fcdc9b4982ffe0b7aac38cc3a630efb0ba183
asad ibrahim <asad.denin@gmail.com>
2025-08-12 19:47:25 +0500

    new  file

--- loops.py
+++ loops.py
@@ -75,3 +75,17 @@
 # else:
 #     print("not pollindrome")
 
+# Set the value of 'n' to 5 (this will determine the number of lines in the pattern)
+n = 5
+
+# Iterate through the range of numbers from 0 to 'n' (exclusive)
+for i in range(n):
+    # Iterate through the range of numbers from 0 to 'i' (exclusive) for each 'i' in the outer loop
+    for j in range(i):
+        # Print '*' followed by a space without a new line (end="" ensures printing in the same line)
+        print('* ', end="")
+    # Move to the next line after printing '*' characters for the current 'i'
+    print('')
+
+
+	
