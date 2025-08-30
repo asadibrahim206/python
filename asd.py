457cca36f06435fc833e6640c4fe24f1e7207cd8
asad ibrahim <asad.denin@gmail.com>
2025-08-30 23:04:35 +0500

    update

--- asd.py
+++ asd.py
@@ -9,14 +9,17 @@
 #     result_str = result_str + "\n"  
 # print(result_str) 
 
-result = ""
-for i in range(0,7):
-    for j in range(0,7):
-        print(j * "*")
-        if(( j== 1 or j == 5 ) and i != 0) or ((i == 0 or i == 3) and (j > 1 and j <5 )):
-            result = result + "*"
-        else:
-            result = result + " "
-    result = result + "\n"
-print(result)
+# result = ""
+# for i in range(0,7):
+#     for j in range(0,7):
+#         print(j * "*")
+#         if(( j== 1 or j == 5 ) and i != 0) or ((i == 0 or i == 3) and (j > 1 and j <5 )):
+#             result = result + "*"
+#         else:
+#             result = result + " "
+#     result = result + "\n"
+# print(result)
+
+for i in range(19):
+    print(i)
         
